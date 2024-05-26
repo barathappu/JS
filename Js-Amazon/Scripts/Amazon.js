ba38379ae43b3e9a0eedae9b865523fcf84dcc11
barathappu <barathbelieve08@gmail.com>
2024-05-26 11:42:41 +0530

    creating html via js

--- Js-Amazon/Scripts/Amazon.js
+++ Js-Amazon/Scripts/Amazon.js
@@ -1,35 +1,37 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
-        stars: 4.5,
-        count: 87
+      stars: 4.5,
+      count: 87,
     },
-    priceCents: 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
+    priceCents: 1090,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
     rating: {
-        stars: 4,
-        count: 127
+      stars: 4,
+      count: 127,
     },
-    priceCents: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+    priceCents: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
-        stars: 4.5,
-        count: 56
+      stars: 4.5,
+      count: 56,
     },
-    priceCents: 799
-}]
+    priceCents: 799,
+  },
+];
 
-let ProductsHtml = '';
+let ProductsHtml = "";
 
-products.forEach(() => {
-    ProductsHtml += ProductsHtml + `
+products.forEach((products) => {
+  ProductsHtml += `
     <div class="product-container">
     <div class="product-image-container">
       <img class="product-image" src="${products.image}">
@@ -40,14 +42,16 @@ products.forEach(() => {
     </div>
 
     <div class="product-rating-container">
-      <img class="product-rating-stars" src="images/ratings/rating-${products.rating.stars * 10}.png">
+      <img class="product-rating-stars" src="images/ratings/rating-${
+        products.rating.stars * 10
+      }.png">
       <div class="product-rating-count link-primary">
         ${products.count}
       </div>
     </div>
 
     <div class="product-price">
-      $${products.priceCents}
+      $${(products.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -78,3 +82,5 @@ products.forEach(() => {
   </div>
     `;
 });
+
+document.querySelector(".product-data").innerHTML = ProductsHtml;
