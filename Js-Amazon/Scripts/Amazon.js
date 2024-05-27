10231ffc39e6b033922f072d137213fd9c3070f4
barathappu <barathbelieve08@gmail.com>
2024-05-27 22:36:26 +0530

    chnages made

--- Js-Amazon/Scripts/Amazon.js
+++ Js-Amazon/Scripts/Amazon.js
@@ -4,7 +4,7 @@ const products = [
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
-      count: 87,
+      count: 87
     },
     priceCents: 1090,
   },
@@ -13,7 +13,7 @@ const products = [
     name: "Intermediate Size Basketball",
     rating: {
       stars: 4,
-      count: 127,
+      count: 127
     },
     priceCents: 2095,
   },
@@ -22,10 +22,19 @@ const products = [
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
       stars: 4.5,
-      count: 56,
+      count: 56
     },
     priceCents: 799,
   },
+  {
+    image: "images/products/black-2-slot-toaster.jpg",
+    name: "2 Slot Toaster - Black",
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 1899,
+  }
 ];
 
 let ProductsHtml = "";
@@ -43,10 +52,9 @@ products.forEach((products) => {
 
     <div class="product-rating-container">
       <img class="product-rating-stars" src="images/ratings/rating-${
-        products.rating.stars * 10
-      }.png">
+        products.rating.stars * 10}.png">
       <div class="product-rating-count link-primary">
-        ${products.count}
+        ${products.rating.count}
       </div>
     </div>
 
